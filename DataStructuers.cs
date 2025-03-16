0f81c207a04cb651a28b262adb5f6d341e77f78e
Austin Scrivener <ostim12121212@gmail.com>
2025-03-17 11:07:02 +1300

    data structures added and made generic

--- DataStructuers.cs
+++ DataStructuers.cs
@@ -6,7 +6,134 @@ using System.Threading.Tasks;
 
 namespace DataStructuers
 {
-    class DataStructuers
+    class Queue<T>
     {
+
+        private T[] queue { get; set; }
+        private int size { get; set; }
+
+        public Queue(int capacity)
+        {
+            queue = new T[capacity];
+            size = 0;
+        }
+
+        public void Enqueue(T input)
+        {
+            if (size == queue.Length)
+                throw new InvalidOperationException("Queue is full");
+
+            queue[size] = input;
+            size++;
+        }
+
+        public T Dequeue()
+        {
+            if (size == 0)
+                throw new InvalidOperationException("Queue is empty");
+
+            T return_item = queue[0];
+
+            for (int i = 1; i < size; i++)
+                queue[i - 1] = queue[i];
+
+            size--;
+            return return_item;
+
+        }
+
+        public T Peek()
+        {
+            if (size == 0)
+                throw new InvalidOperationException("Queue is empty");
+
+            return queue[0];
+        }
+
+
+    }
+
+    class IntStack<T>
+    {
+        private T[] stack { get; set; }
+        private int size { get; set; }
+
+        public IntStack(int capacity)
+        {
+            stack = new T[capacity];
+            size = 0;
+        }
+
+        public void Push(T input)
+        {
+            if (size == stack.Length)
+                throw new InvalidOperationException("Stack is full");
+
+            stack[size] = input;
+            size++;
+        }
+
+        public T Pop()
+        {
+            if (size == 0)
+                throw new InvalidOperationException("Stack is empty");
+
+            T return_item = stack[size - 1];
+            size--;
+            return return_item;
+
+        }
+
+        public T Peek()
+        {
+            if (size == 0)
+                throw new InvalidOperationException("Stack is empty");
+
+            return stack[size - 1];
+        }
+    }
+
+    class SimpleHashTable<T>
+    {
+        private int size = 10;
+        private string[] keys;
+        private T[] values;
+
+        public SimpleHashTable()
+        {
+            keys = new string[size];
+            values = new T[size];
+        }
+
+        private int GetHash(string key)
+        {
+            int hash = 0;
+            foreach (char c in key)
+            {
+                hash += c;
+            }
+            return hash % size;
+        }
+
+        public void Insert(string key, T value)
+        {
+            int index = GetHash(key);
+
+            if (keys[index] == key || keys[index] == null)
+            {
+                keys[index] = key;
+                values[index] = value;
+            }
+            else
+            {
+                throw new InvalidOperationException("Hash collision occurred");
+            }
+        }
+
+        public T GetValue(string key)
+        {
+            int index = GetHash(key);
+            return values[index];
+        }
     }
 }
--- Sorting.cs
+++ Sorting.cs
@@ -6,7 +6,33 @@ using System.Threading.Tasks;
 
 namespace Sorting
 {
-    class Class1
+    public class LinearSearch<T>
     {
+        public static int Search(T[] arr, T key)
+        {
+            for (int i = 0; i < arr.Length; i++)
+            {
+                if (Comparison<T>.Equals(arr[i], key)) return i;
+            }
+            return -1;
+        }
+
+    }
+    public class BinarySearch<T> where T : IComparable<T>
+    {
+        public static int Search(T[] arr, T key)
+        {
+            int left = 0, right = arr.Length - 1;
+            while (left <= right)
+            {
+                int mid = left + (right - left) / 2;
+                // Found the key, return its position
+                if (Comparison<T>.Equals(arr[mid], key)) return mid;
+                // If the middle element is less than the key, search in the right hal
+                if ((key.CompareTo(arr[mid])) > 0) left = mid + 1;
+                else right = mid - 1; // Otherwise, search in the left half
+            }
+            return -1; // negative index signals not found
+        }
     }
 }
