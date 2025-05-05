67e45b7236bc09ae5d7c006012d72c378d987520
Austin Scrivener <ostim12121212@gmail.com>
2025-05-05 12:27:57 +1200

    Rebased to Add testing
    moved all program files to their own folder to allow me to have testing in the same repo

---
+++ D202_Assignment_1_tests/UnitTest1.cs
@@ -0,0 +1,72 @@
+using D202_assignment_1;
+using MovieClasses;
+
+namespace TestIng
+{
+    public class UnitTestSorting
+    {
+        [Fact]
+        public void SortListOfMoviesByIDTest()
+        {
+            MovieList testList = new MovieList();
+
+            Movie testMovie1 = new Movie();
+
+            for (int i = 10; i > 0; i--)
+            {
+                testMovie1.ID = Convert.ToString(i);
+                testMovie1.Title = "Title" + i;
+                testList.Add(testMovie1);
+            }
+
+            MovieList SortListOfMoviesByID(MovieList ListOfMovies)
+            {
+                SortListOfMoviesByID soreter = new SortListOfMoviesByID();
+                ListOfMovies = soreter.SortByID(ListOfMovies);
+                return ListOfMovies;
+            }
+
+            SortListOfMoviesByID(testList);
+
+            for (int i = 0; i > 10; i++)
+            {
+                Assert.Equal(Convert.ToString(i), testList.Movies[i].ID);
+            }
+        }
+
+        [Fact]
+        public void SortListOfMoviesByTitleTest()
+        {
+            MovieList testList = new MovieList();
+
+            Movie testMovie1 = new Movie();
+
+            for (int i = 10; i > 0; i--)
+            {
+                testMovie1.ID = Convert.ToString(i);
+                testMovie1.Title = "Title" + i;
+                testList.Add(testMovie1);
+            }
+
+            MovieList SortListOfMoviesByTitle(MovieList ListOfMovies)
+            {
+                SortListOfMoviesByTitle soreter = new SortListOfMoviesByTitle();
+                ListOfMovies = soreter.SortByTitle(ListOfMovies);
+                return ListOfMovies;
+            }
+
+            SortListOfMoviesByTitle(testList);
+
+            for (int i = 0; i > 10; i++)
+            {
+                Assert.Equal("Title" + i, testList.Movies[i].Title);
+            }
+        }
+
+    }
+
+    public class UnitTestSearching
+    {
+
+    }
+}
