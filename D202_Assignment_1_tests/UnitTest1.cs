4f35e9b8e0846288cbce97efdc83255235cf6364
Austin Scrivener <ostim12121212@gmail.com>
2025-05-12 00:03:12 +1200

    fixed unit tests

--- D202_Assignment_1_tests/UnitTest1.cs
+++ D202_Assignment_1_tests/UnitTest1.cs
@@ -1,5 +1,6 @@
 using D202_assignment_1;
 using MovieClasses;
+using search;
 
 namespace TestIng
 {
@@ -10,20 +11,21 @@ namespace TestIng
         {
             MovieList testList = new MovieList();
 
-            Movie testMovie1 = new Movie();
+           
 
-            for (int i = 10; i > 0; i--)
+            for (int i = 9; i > 0; i--)
             {
+                Movie testMovie1 = new Movie();
                 testMovie1.ID = Convert.ToString(i);
                 testMovie1.Title = "Title" + i;
                 testList.Add(testMovie1);
             }
 
-            SortListOfMoviesByID.SortByID(testList);
+            testList = SortListOfMoviesByID.SortByID(testList);
 
-            for (int i = 0; i > 10; i++)
+            for (int i = 1; i < 10; i++)
             {
-                Assert.Equal(Convert.ToString(i), testList.Movies[i].ID);
+                Assert.Equal(Convert.ToString(i), testList.Movies[i-1].ID);
             }
         }
 
@@ -32,20 +34,21 @@ namespace TestIng
         {
             MovieList testList = new MovieList();
 
-            Movie testMovie1 = new Movie();
+            
 
-            for (int i = 10; i > 0; i--)
+            for (int i = 9; i > 0; i--)
             {
+                Movie testMovie1 = new Movie();
                 testMovie1.ID = Convert.ToString(i);
                 testMovie1.Title = "Title" + i;
                 testList.Add(testMovie1);
             }
 
-            SortListOfMoviesByTitle.SortByTitle(testList);
+            testList = SortListOfMoviesByTitle.SortByTitle(testList);
 
-            for (int i = 0; i > 10; i++)
+            for (int i = 1; i < 10; i++)
             {
-                Assert.Equal("Title" + i, testList.Movies[i].Title);
+                Assert.Equal("Title" + i, testList.Movies[i - 1].Title);
             }
         }
 
@@ -53,6 +56,66 @@ namespace TestIng
 
     public class UnitTestSearching
     {
+        [Fact]
+        public void SearchByTitle()
+        {
+            MovieList testList = new MovieList();
 
+            // id , title , pos
+            // 9 , Title9 , 0
+            // 8 , Title8 , 1
+            // 7 , Title7 , 2
+            // 6 , Title6 , 3
+
+            for (int i = 9; i > 0; i--)
+            {
+                Movie testMovie1 = new Movie();
+                testMovie1.ID = Convert.ToString(i);
+                testMovie1.Title = "Title" + i;
+                testList.Add(testMovie1);
+            }
+
+            MovieList outputlist = new MovieList();
+
+            int outputIndexNum = -1;
+            
+            (outputlist,outputIndexNum) = SearchBYTitle.search(testList, "Title6");
+
+            Assert.Equal(3, outputIndexNum);
+            Assert.Equal("Title6", outputlist.Movies[0].Title);
+
+        }
+        [Fact]
+        public void SearchBYID()
+        {
+            MovieList testList = new MovieList();
+
+            // all strings
+            // id , title , pos
+            // 0 , Title0 , 0
+            // 1 , Title1 , 1
+            // 2 , Title2 , 2
+            // 3 , Title3 , 3
+            //... goes to 9
+
+            for (int i = 0; i < 10; i++)
+            {
+                Movie testMovie1 = new Movie();
+                testMovie1.ID = Convert.ToString(i);
+                testMovie1.Title = "Title" + i;
+                testList.Add(testMovie1);
+            }
+            testList.IsSortedByID = true;
+
+            MovieList outputlist = new MovieList();
+
+            int outputIndexNum = -1;
+
+            (outputlist, outputIndexNum) = SearchByID.search(testList, "5");
+
+            Assert.Equal(5, outputIndexNum);
+            Assert.Equal("5", outputlist.Movies[0].ID);
+
+        }
     }
 }
